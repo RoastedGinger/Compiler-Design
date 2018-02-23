ee6d5f05dcc7d238009b12c0871bbe78ad55ba2f
RoastedGinger <robingurung652@gmail.com>
2018-02-23 11:25:10 +0530

    Update COMMENT.C
    final 
    no other changes will be done

--- COMMENT.C
+++ COMMENT.C
@@ -2,20 +2,21 @@
 # include<conio.h>
 void main()
 {
-  char comment[10];
-  int i,j,flag=0;
+  char comment[100];
+  int i,j,flag=0,slag=0;
   printf("enter comment \n");
   gets(comment);
-      for(i=0;i<=9;i++)
+      for(i=0;i<=100;i++)
       {
        if(comment[i] == '/' && comment[i+1] == '/') {
+       slag=1;
        printf("comment encountered in %d and %d index",i,i+1);
        break;
        }
 	 else if(comment[i] == '/' && comment[i+1] == '*')
 	      {
 
-		   for(j=i+2;j<=9;j++)
+		   for(j=i+2;j<=100;j++)
 		    {
 		     if(comment[j] == '*' && comment[j+1] == '/')
 		      {
@@ -25,13 +26,20 @@ void main()
 		      }
 		    }
 		  if(flag==1)
-		  printf("its a comment");
+		  {
+		  printf("its a comment\n");
+		  goto x;
+		  }
 		  else
-		  printf("its not comment");
+		  printf("its not comment\n");
 
 	      break;
 	      }
        }
+	if(slag==1)
+	printf("comment\n");
+	else
+	printf("not a comment");
  // printf(comment);
-  getch();
+x :  getch();
 }
